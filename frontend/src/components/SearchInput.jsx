8a97b313d05cc3348563394287b0c0143a69d83a
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-09 18:54:02 +0900

    💄 update ui

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { AiOutlineSearch } from "react-icons/ai";
 import { IoMdClose } from "react-icons/io";
-import MicIcon from "../assets/mic.svg";
-import ImageIcon from "../assets/image.svg";
 
 const SearchInput = () => {
   const { query } = useParams();
@@ -19,9 +16,9 @@ const SearchInput = () => {
   return (
     <div
       id="searchBox"
-      className="h-[46px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0"
+      className="h-[20px] w-[250px] flex items-center gap-3 px-4 border border-[#9A9A9A] hover:bg-white"
     >
-      <AiOutlineSearch size={18} color="#9aa0a6" />
+      {/* h-[46px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl */}
       <input
         type="text"
         className="grow outline-0 text-black/[0.87]"
@@ -39,8 +36,6 @@ const SearchInput = () => {
             onClick={() => setSearchQuery("")}
           />
         )}
-        <img src={MicIcon} alt="mic-icon" className="h-6 w-6" />
-        <img src={ImageIcon} alt="image-icon" className="h-6 w-6" />
       </div>
     </div>
   );
