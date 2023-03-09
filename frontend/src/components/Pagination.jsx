f1949033cae1f39e7cab34c33ca1676484b3eba8
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-10 00:44:10 +0900

    💄 update ui

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -21,30 +21,20 @@ const Pagination = ({ queries }) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center pt-10">
-      <div className="relative text-[#629889] underline">
+    <div className="flex flex-col justify-center items-center mt-3">
+      <div className="relative text-[#629889]">
         {queries.previousPage && (
           <div
             className="absolute left-[-30px] md:left-[-40px] top-[10px]"
             onClick={() => paginationClickHandler(queries.previousPage[0].startIndex)}
-          >
-            <FiChevronLeft size={20} className="cursor-pointer" />
-            <span className="cursor-pointer absolute left-[-5px] top-[30px] hidden md:block">
-              Prev page
-            </span>
-          </div>
+          ></div>
         )}
         <img src={Logo} alt="logo-image" className="w-[250px] md:w-[300px]" />
         {queries.nextPage && (
           <div
             className="absolute right-[-30px] md:right-[-40px] top-[10px]"
             onClick={() => paginationClickHandler(queries.nextPage[0].startIndex)}
-          >
-            <FiChevronRight size={20} className="cursor-pointer" />
-            <span className="cursor-pointer absolute left-[-5px] top-[30px] hidden md:block">
-              Next page
-            </span>
-          </div>
+          ></div>
         )}
       </div>
       <div className="flex gap-3 text-[#2914E8] text-sm">
