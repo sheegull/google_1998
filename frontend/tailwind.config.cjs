bd577515536a41776e26cbf9bbcb10e4cafc14d9
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-09 23:19:30 +0900

    🔧 add fontFamily

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,9 +3,8 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      boxShadow: {
-        c: "0 1px 6px rgb(32 33 36 / 28%)",
-        c2: "0 1px 1px rgb(0 0 0 / 10%)",
+      fontFamily: {
+        serif: ["Georgia", "Cambria", "Times New Roman", "serif"],
       },
     },
   },
