58d305f48d105c7b7a517c20be0ab5f9214cef77
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-09 23:55:14 +0900

    💄 update searchItem ui

--- frontend/src/components/SearchedItem.jsx
+++ frontend/src/components/SearchedItem.jsx
@@ -10,13 +10,13 @@ const SearchedItem = ({ data }) => {
   return (
     <div className="flex flex-col py-3 max-w-[700px]">
       <div className="group cursor-pointer" onClick={() => window.open(data.link, "_blank")}>
-        <div className="text-sm truncate text-[#202124]">{data.formattedUrl}</div>
-        <div className="group-hover:underline text-xl text-[#1a0dab] pt-2">{data.title}</div>
+        <div className="underline text-xl text-[#2914E8] border-b-1 pt-2">{data.title}</div>
       </div>
       <div
         className="text-sm text-[#4d5156] leading-6 pt-1"
         dangerouslySetInnerHTML={createMarkup(data.htmlSnippet)}
       />
+      <div className="text-sm truncate text-[#629889]">{data.formattedUrl}</div>
     </div>
   );
 };
