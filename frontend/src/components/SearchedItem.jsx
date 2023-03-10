d4960c509d7fe71a4c7168abe4dc2fe08cd0a594
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-10 15:08:50 +0900

    💄 change data.snippet

--- frontend/src/components/SearchedItem.jsx
+++ frontend/src/components/SearchedItem.jsx
@@ -1,21 +1,10 @@
 const SearchedItem = ({ data }) => {
-  // function createMarkup(html) {
-  //   return { __html: html };
-  // }
-
-  const createMarkup = (html) => {
-    return { __html: html };
-  };
-
   return (
     <div className="flex flex-col py-3 max-w-[700px]">
       <div className="group cursor-pointer" onClick={() => window.open(data.link, "_blank")}>
         <div className="underline text-xl text-[#2914E8] border-b-1 pt-2">{data.title}</div>
       </div>
-      <div
-        className="text-sm text-[#4d5156] leading-6 pt-1"
-        dangerouslySetInnerHTML={createMarkup(data.htmlSnippet)}
-      />
+      <div className="text-sm text-[#4d5156] leading-6 pt-1">...{data.snippet}</div>
       <div className="text-sm truncate text-[#629889]">{data.formattedUrl}</div>
     </div>
   );
