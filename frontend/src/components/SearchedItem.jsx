cfa4badccecd4827cb9d2d3e3aadfb74883e1a75
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-10 16:30:34 +0900

    💄 update ui

--- frontend/src/components/SearchedItem.jsx
+++ frontend/src/components/SearchedItem.jsx
@@ -4,7 +4,7 @@ const SearchedItem = ({ data }) => {
       <div className="group cursor-pointer" onClick={() => window.open(data.link, "_blank")}>
         <div className="underline text-xl text-[#2914E8] border-b-1 pt-2">{data.title}</div>
       </div>
-      <div className="text-sm text-[#4d5156] leading-6 pt-1">...{data.snippet}</div>
+      <div className="text-sm text-[#4d5156] leading-6 pt-1">{data.snippet}</div>
       <div className="text-sm truncate text-[#629889]">{data.formattedUrl}</div>
     </div>
   );
