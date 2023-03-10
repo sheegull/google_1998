842c931155b051b3017a3b6242b04cbfc2913ceb
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-10 17:12:59 +0900

    🎨 format

--- frontend/src/utils/Constants.jsx
+++ frontend/src/utils/Constants.jsx
@@ -1,21 +1,3 @@
-import { GoSearch } from "react-icons/go";
-import { BsImage } from "react-icons/bs";
-import { BiNews } from "react-icons/bi";
-import { RiVideoLine } from "react-icons/ri";
-import { SlTag } from "react-icons/sl";
-
-export const quickLinks = ["About", "Advertising", "Business", "How Search works"];
-
-export const settingMenu = ["Privacy", "Terms", "Settings"];
-
-export const menu = [
-  { name: "All", icon: <GoSearch /> },
-  { name: "Images", icon: <BsImage size={14} /> },
-  { name: "News", icon: <BiNews /> },
-  { name: "Videos", icon: <RiVideoLine /> },
-  { name: "Shopping", icon: <SlTag size={14} /> },
-];
-
 export const pagination = [
   { page: 1, startIndex: 1 },
   { page: 2, startIndex: 11 },
