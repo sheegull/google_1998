f8a1278ea09b881b89d8495faeae0025e31f6e48
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-10 16:31:21 +0900

    🎨 format

--- frontend/src/components/SearchResultHeader.jsx
+++ frontend/src/components/SearchResultHeader.jsx
@@ -1,26 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-// import { menu } from "../utils/Constants";
 import { Context } from "../utils/ContextApi";
 import SearchInput from "./SearchInput";
-// import ProfileIcon from "./ProfileIcon";
 
 import Logo from "../assets/google_1998_logo.png";
 
 const SearchResultHeader = () => {
-  const [selectedMenu, setSelectedMenu] = useState("All");
   const { setImageSearch } = useContext(Context);
 
   useEffect(() => {
     return () => setImageSearch(false);
   }, []);
-
-  // const clickHandler = (menuItem) => {
-  //   let isTypeImage = menuItem.name === "Images";
-  //   setImageSearch(isTypeImage ? true : false);
-  //   setSelectedMenu(menuItem.name);
-  // };
 
   return (
     <div className="p-[15px] pb-0 md:pr-5 md:pl-20 md:pt-7 border-b border-[#ebebeb] flex md:block flex-col items-center top-0 bg-white">
@@ -31,11 +22,11 @@ const SearchResultHeader = () => {
           </Link>
           <div className="flex justify-center items-center gap-2">
             <SearchInput from="SearchResult" />
-            <button className="h-5 px-1 md:px-2 flex items-center  bg-[#FCFCFC] text-[11px] border border-[#AFAFAF] rounded-md">
-              Google Search
-            </button>
             <button className="h-5 px-2 flex items-center  bg-[#FCFCFC] text-[11px] border border-[#AFAFAF] rounded-md">
-              I'm feeling lucky
+              <span className="hidden md:block">Google&nbsp;</span>Search
+            </button>
+            <button className="flex h-5 px-2 items-center  bg-[#FCFCFC] text-[11px] border border-[#AFAFAF] rounded-md">
+              <span className="hidden md:block">I'm feeling&nbsp;</span>lucky
             </button>
           </div>
         </div>
@@ -43,24 +34,6 @@ const SearchResultHeader = () => {
           Help
         </div>
       </div>
-
-      {/* <div className="flex ml-[-12px] mt-3">
-        {menu.map((menu, idx) => (
-          <span
-            key={idx}
-            className={`flex items-center p-3 text-[#5f6368] cursor-pointer relative ${
-              selectedMenu === menu.name ? "text-[#1a73e8]" : ""
-            }`}
-            onClick={() => clickHandler(menu)}
-          >
-            <span className="hidden md:block mr-2">{menu.icon}</span>
-            <span className="text-sm">{menu.name}</span>
-            {selectedMenu === menu.name && (
-              <span className="h-[3px] w-[calc(100%-20px)] absolute bg-[#1a73e8] bottom-0 left-[10px]" />
-            )}
-          </span>
-        ))}
-      </div> */}
     </div>
   );
 };
